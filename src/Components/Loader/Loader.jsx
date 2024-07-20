85b5a83c2650d89c7c2d1a736c1c376c510f374b
Davidlifelover <david.majorov04@gmail.ru>
2024-07-20 03:09:40 +0300

    first

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import { Spin } from 'antd';
+import './Loader.css';
+
+export default function Loader() {
+    return (
+        <div className="example">
+            <Spin className="loader" tip="Loading...">
+                <div className="content" />
+            </Spin>
+        </div>
+        
+    );
+}
